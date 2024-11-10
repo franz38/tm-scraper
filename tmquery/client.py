6ccb79a5c9998360516e296dfcac3feb7237eb2e
franz <mattefranzz@gmail.com>
2024-11-10 16:39:26 +0100

    client fix

--- tmquery/client.py
+++ tmquery/client.py
@@ -1,37 +1,42 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-from typing import Dict
 from utils.singleton import Singleton
 import os
+
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 features = "html.parser"
 
+
 class Client(metaclass=Singleton):
     
-    cache_dir: str = "./cache/"
     cache_results: bool
-    scraped: Dict[str, str] = {}
+    cache_dir: str
 
-    def __init__(self, cache_results: bool = False):
+    def __init__(self, cache_results: bool = False, cache_dir: str = "./cache/"):
         self.cache_results = cache_results
+        self.cache_dir = cache_dir
 
 
     def fetch_cache(self, url: str) -> str:
         filename = self.cache_dir + url
+        data = ""
 
-        if not os.path.isfile(filename):
-            html_page: str = requests.get(url, headers=headers).text
-            os.makedirs(os.path.dirname(filename), exist_ok=True)
-            with open(filename, "w") as file:
-                file.write(html_page)
+        if self.cache_results:
+            if not os.path.isfile(filename):
+                html_page: str = requests.get(url, headers=headers).text
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                with open(filename, "w") as file:
+                    file.write(html_page)
 
-        if url not in self.scraped:
             with open(filename, "r") as file:
-                self.scraped[url] = file.read()
-
-        return self.scraped[url]
+                data = file.read()
+        else:
+            data = requests.get(url, headers=headers).text
+        
+        return data
+    
 
     def scrape(self, url: str) -> 'BeautifulSoup':
         
